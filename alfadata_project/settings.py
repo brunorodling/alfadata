c2896066ca64d6fc13519ee86025abb2dc15d23e
brunorodling <rodlingb796@gmail.com>
2025-11-08 21:35:11 -0300

    Fix: add WhiteNoise + set Python 3.11 for Render deploy

--- alfadata_project/settings.py
+++ alfadata_project/settings.py
@@ -59,6 +59,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
